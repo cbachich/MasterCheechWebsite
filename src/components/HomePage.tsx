115948a1827bd9c71a8cc95fcc5e7d022f71c548
Christopher Bachicha <cbachich@gmail.com>
2023-07-30 00:01:11 -0600

    Add Material UI

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,9 +1,11 @@
 import React, { FunctionComponent } from 'react';
+import Button from '@mui/material/Button';
 
 const HomePage: FunctionComponent = () => {
   return (
     <div>
       <h1>Christopher Bachicha</h1>
+      <Button variant="contained">Hello World</Button>
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import HomePage from './components/HomePage';
 import AboutMePage from './components/AboutMePage';
 import BlogPage from './components/BlogPage';
